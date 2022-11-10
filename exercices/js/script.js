b1ca1f0c0f87c448b58b1f77d0bbf52841680fcc
Abdoulaye <abdelraul140385@gmail.com>
2022-11-10 15:58:46 +0000

    exos javascript

---
+++ exercices/js/script.js
@@ -0,0 +1,36 @@
+// exercice1:
+// const user ={
+//                name : "oumar",
+//                password : "@oumar123"
+// }
+
+// let mp = prompt("Entrez votre mot de passe SVP");
+
+// if (user.password == mp){
+//                alert("Bienvenue M. " + user.name);
+// }else{
+//                alert("erreur dans le mot de passe")
+// }
+
+// exercice2 :
+//           X = parseInt(prompt("Entrez un nombre entier SVP"));
+//           for(let i=0 ; i<=X ;i++){
+//                p = 2*i
+//                console.log(p)
+//           }
+
+//           Y = parseInt(prompt("Entrez un nombre entier SVP"));
+//           for(let i=0 ; i<=Y ;i++){
+//                k = 2*i + 1
+//                console.log(k)
+//           }
+
+// Exercice3:
+
+function interChanger(a,b){
+               a = parseInt(prompt("entrez un nombre a :"));
+               b = parseInt(prompt("entrez un nombre b :"));
+               
+}
+
+         
